7c4478cdc38606ffb1d6736d46fd82590cef2d3d
TA-800 <taherali0905@gmail.com>
2026-02-25 19:27:40 -0500

    simple api to test json response, remove sql.txt

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ app.get("/", (req, resp) => {
     resp.sendFile(path.join(__dirname, pathToUi, "index.html"));
 });
 
+app.get("/data", (req, resp) => {
+    // Sends a JSON response
+    // https://expressjs.com/en/5x/api.html#res.json
+    resp.json({
+        data: "Hello World",
+    });
+    // This way we can return data from the database as a JSON to the frontend
+    // E.g. /<courseId> returns list of reviews for <courseId> from the database
+});
+
 // Start the server
 app.listen(8080, () => {
     console.log("Server.js listening on port 8080.");
