23d2d2b454508ccd53eb3b56c531a28f40656a32
TA-800 <taherali0905@gmail.com>
2026-04-01 17:16:26 -0400

    add login/register

---
+++ frontend/src/UserContext.js
@@ -0,0 +1,10 @@
+import React, { useState, createContext } from "react";
+
+const UserContext = createContext(null);
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,15 @@
-/*Navbar components*/
-import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 /*navbar links to homepage*/
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <Link to="/" className="navbar-brand">
-        WLU CS Course Tracker
-      </Link>
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/">About</Link>
-        <Link to="/">Login / Register</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/">Home</Link>
+        </nav>
+    );
 }
 
 export default Navbar;
